41e044f2b483279df540178a69adf8c152c2a5a6
Omar Ezz Ed-Dien <om4rezz@linux.ninja.box>
2018-02-04 22:08:08 +0200

    Build Subscription model and register it with the admin board to make a customization

--- subscriptions/admin.py
+++ subscriptions/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import Subscription
+
 # Register your models here.
+
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ['__unicode__', 'timestamp', 'updated']
+    class Meta:
+        model = Subscription
+
+admin.site.register(Subscription, SubscriptionAdmin)
